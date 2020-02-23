1e47058df642b675fc2f526aedc5b4b0c94e4364
fengqinglingyu <liang823740098@hotmail.com>
2020-02-23 19:24:50 +0800

    修改 gitignore

---
+++ build/precache-manifest.d9344af1c4a0839b1374566d6be3e27f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000bfc5f3aca7de69a0c6c5f3c395984",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2650b5bc44178231249",
+    "url": "/static/css/2.fa7fdd6a.chunk.css"
+  },
+  {
+    "revision": "6528e3d92f3c09dff06f",
+    "url": "/static/css/main.9e5c1a88.chunk.css"
+  },
+  {
+    "revision": "a2650b5bc44178231249",
+    "url": "/static/js/2.9fc55fee.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/static/js/2.9fc55fee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6528e3d92f3c09dff06f",
+    "url": "/static/js/main.6cb857cb.chunk.js"
+  },
+  {
+    "revision": "e4de161b2c01cc3baafb",
+    "url": "/static/js/runtime-main.005cd164.js"
+  }
+]);
